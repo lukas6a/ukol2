dbb9566cc1edc848e9b5d0e7a013e6ad37fc3bee
L. Petrbok <lukas@google.com>
2015-11-08 18:28:18 +0100

    first

---
+++ app/src/main/java/pef/kit/hotel/main2.java
@@ -0,0 +1,33 @@
+package pef.kit.hotel;
+
+/**
+ * Created by Lukas on 8.11.2015.
+ */
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class main2 extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+
+}
